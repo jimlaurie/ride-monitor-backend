f92b41c7fc67b2cbe15b170160bf8d62c435fcdd
jimlaurie <jimlaurie@gmail.com>
2025-10-27 16:47:48 -0700

    Fix ride.status3

--- server.js
+++ server.js
@@ -130,18 +130,6 @@ function organizeParkData(parkData, landMap) {
       paidReturnPrice: null,
       paidStandbyWait: null
     };
-      // Map status from API
-      if (liveData.status === 'OPERATING') {
-        ride.status = 'OPERATING';
-      } else if (liveData.status === 'DOWN') {
-        ride.status = 'DOWN';
-      } else if (liveData.status === 'REFURBISHMENT') {
-        ride.status = 'REFURBISHMENT';
-      } else if (liveData.status === 'CLOSED') {
-        ride.status = 'CLOSED';
-      } else {
-        ride.status = liveData.status || 'CLOSED';
-      }
 
     // Add live wait time if available
     if (liveData && liveData.queue) {
